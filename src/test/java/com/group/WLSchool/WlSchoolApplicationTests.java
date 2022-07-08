78a6ca749919594edcf1bd373449322c368ac0c5
victor <joaovictor1c2013@gmail.com>
2022-07-08 14:24:57 -0300

    primeiro commit

---
+++ src/test/java/com/group/WLSchool/WlSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.WLSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
